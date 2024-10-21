c64810e5e507a39758265279a38a6e11b4ce2c53
Joel March <joel.march01@estudiant.upf.edu>
2024-10-21 11:51:11 +0200

    Primera

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,17 +1,56 @@
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
-def main() :
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
 
+def count_divisors(n):
+    count = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            count += 1
+    return count
+
+def is_antiprime(n):
+    max_divisors = count_divisors(n)
+    for i in range(1, n):
+        if count_divisors(i) >= max_divisors:
+            return False
+    return True
+
+def main():
+    x = int(input("Introdueix un nombre enter positiu: "))
+    if x <= 0:
+        print("Si us plau, introdueix un nombre enter positiu.")
+        return "not anti-prime"  # Return value if the input is invalid
+    if is_antiprime(x):
+        return "anti-prime"
+    else:
+        return "not anti-prime"
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 1:
+        try:
+            x = int(sys.argv[1])
+            if x <= 0:
+                print("Si us plau, introdueix un nombre enter positiu.")
+            else:
+                if is_antiprime(x):
+                    print("anti-prime")
+                else:
+                    print("not anti-prime")
+        except ValueError:
+            print("Please provide a valid integer.")
+    else:
+        print(main())
 
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
 	## RETURN THE VALUE "anti-prime" or "not anti-prime"
 	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
 	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
 	## "anti-prime" or "not anti-prime"
-	return("anti-prime")
+	
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
